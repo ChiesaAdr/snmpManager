e2ae460d4eb8cb917fc1ccaafa93a0e8bdc8815b
Adriano Chiesa <citeb.testes@citeb.com.br>
2021-10-01 17:22:17 -0300

    Nem test

--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	// resources "github.com/ChiesaAdr/snmpManager/resourcesSnmp"
+	resources "github.com/ChiesaAdr/snmpManager/resourcesSnmp"
 
 	"github.com/gosnmp/gosnmp"
 )
 
 //TODO: Test this with snmpMocker (https://git.intelbras.com.br/olt-software/snmpmocker) project
 func testTrapListener() {
-	// file := os.Stdout
+	file := os.Stdout
 	conn := gosnmp.Default
 	conn.Target = "10.100.34.66"
 	tl := gosnmp.NewTrapListener()
-	// tl.OnNewTrap = resources.MyTrapHandler(file)
+	tl.OnNewTrap = resources.MyTrapHandler(file)
 	tl.Params = conn
 	err := tl.Listen("0.0.0.0:162")
 	if err != nil {
